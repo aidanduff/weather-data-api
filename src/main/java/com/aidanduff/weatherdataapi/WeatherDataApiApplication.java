f2327dd760256e3344ef7227980bfee50bad4ce5
Aidan Duff <aidanpatrickduff@gmail.com>
2020-08-10 13:45:20 +0100

    Implemented file uploading on server side

--- src/main/java/com/aidanduff/weatherdataapi/WeatherDataApiApplication.java
+++ src/main/java/com/aidanduff/weatherdataapi/WeatherDataApiApplication.java
@@ -1,15 +1,28 @@
 package com.aidanduff.weatherdataapi;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aidanduff.weatherdataapi.service.FilesStorageService;
+
 @SpringBootApplication
 @EnableAutoConfiguration
-public class WeatherDataApiApplication {
+public class WeatherDataApiApplication implements CommandLineRunner {
+	  @Resource
+	  FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherDataApiApplication.class, args);
 	}
+	
+	@Override
+	  public void run(String... arg) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+	  }
 
 }
