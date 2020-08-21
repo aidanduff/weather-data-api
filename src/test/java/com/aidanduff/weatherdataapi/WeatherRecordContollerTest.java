030f9b9a716d2b182b394a9e6d2bc72da1976ec1
Aidan Duff <aidanpatrickduff@gmail.com>
2020-08-21 14:47:22 +0100

    Added more controller tests.

--- src/test/java/com/aidanduff/weatherdataapi/WeatherRecordContollerTest.java
+++ src/test/java/com/aidanduff/weatherdataapi/WeatherRecordContollerTest.java
@@ -1,6 +1,8 @@
 package com.aidanduff.weatherdataapi;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -60,6 +62,39 @@ public class WeatherRecordContollerTest {
 	
 		assertEquals(ArrayList.class, weatherRecordService.getAllWeatherRecords().getClass());
 		
+	}
+	
+	@Test
+	public void listReturnedFromGetAllShouldBeSpecificSize() throws Exception {
+		WeatherRecord weatherRecord = new WeatherRecord(1985, "Dec", 10, 17.5, 12.5, 5, 8);
+		weatherRecord.setStationLocation("Athlone");
+		WeatherRecord secondWeatherRecord = new WeatherRecord(1986, "Jan", 11, 18.5, 11.5, 4, 9);
+		weatherRecord.setStationLocation("Athlone");
+		List<WeatherRecord> weatherRecordList = new ArrayList<>();
+		weatherRecordList.add(weatherRecord);
+		weatherRecordList.add(secondWeatherRecord);
+
+		when(weatherRecordService.getAllWeatherRecords()).thenReturn(weatherRecordList);
+		
+		this.mockMvc.perform(get("/weather"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON));	
+		
+		assertEquals(2, weatherRecordService.getAllWeatherRecords().size());
+	}
+	
+	@Test
+	public void getAllResultShouldHaveNoContent() throws Exception {
+		List<WeatherRecord> weatherRecordList = new ArrayList<>();
+		
+		when(weatherRecordService.getAllWeatherRecords()).thenReturn(weatherRecordList);
+		
+		this.mockMvc.perform(get("/weather"))
+			.andDo(print())
+			.andExpect(status().isNoContent());
+		
+		assertTrue(weatherRecordService.getAllWeatherRecords().isEmpty());
 	}
 
 }
