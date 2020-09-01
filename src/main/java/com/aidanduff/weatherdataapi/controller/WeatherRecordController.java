92a328391b5994e80c641e3abba482fd663b9aca
Aidan Duff <aidanpatrickduff@gmail.com>
2020-09-01 21:19:31 +0100

    reverted CORS policy

--- src/main/java/com/aidanduff/weatherdataapi/controller/WeatherRecordController.java
+++ src/main/java/com/aidanduff/weatherdataapi/controller/WeatherRecordController.java
@@ -14,7 +14,7 @@ import com.aidanduff.weatherdataapi.entity.WeatherRecord;
 import com.aidanduff.weatherdataapi.service.FilesStorageServiceImpl;
 import com.aidanduff.weatherdataapi.service.WeatherRecordService;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200", "https://rainydays-server.herokuapp.com" })
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 public class WeatherRecordController {
 
